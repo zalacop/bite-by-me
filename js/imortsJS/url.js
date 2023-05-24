a044be7b453bb4ffa0f04a8a4f96fca5f3c308f3
zalacop <103320914+zalacop@users.noreply.github.com>
2023-05-24 20:02:39 +0200

    made the show more button functional on recipes-list page

--- js/imortsJS/url.js
+++ js/imortsJS/url.js
@@ -2,8 +2,10 @@
 export const host = "https://www.webupload.no";
 export const wordPress = "/wp-json/wp/v2";
 export const posts = "/posts";
-export const endpoint = "?_embed&per_page=20";
+export const endpoint = "?_embed&";
 export const featured = "?_embed&tags=4";
 
 export const urlPosts = host + wordPress + posts + endpoint; 
 export const urlFeatured = host + wordPress + posts + featured;
+
+// https://www.webupload.no/wp-json/wp/v2/posts?_embed&per_page=10
--- js/recipes.js
+++ js/recipes.js
@@ -1,30 +1,29 @@
 import { urlPosts } from "./imortsJS/url.js";
 
 const recipesContainer = document.querySelector(".recipes_list");
+const showMore = document.querySelector(".show-more");
 
-async function getRecipesPosts() {
+async function fetchRecipes(pageNumber, numberOfRecipes) {
 
     // fetching the JSON and catching error message
     try {
-        const getPosts = await fetch(urlPosts);
+        const getPosts = await fetch(urlPosts + `&per_page=${numberOfRecipes}&page=${pageNumber}`);
         const recipes = await getPosts.json();
+        
+        const recipePosts = recipes.map((recipe) => {
+            const data = {
+                "id": recipe.id,
+                "imageURL": recipe._embedded["wp:featuredmedia"][0].source_url,
+                "imageAltText": recipe._embedded["wp:featuredmedia"][0].alt_text,
+                "title": recipe.title.rendered
+            }
+            
+            return data;
+        } )
 
-        recipesContainer.innerHTML = " ";
-    
-        for (let i = 0; i < recipes.length; i++) {
-            const recipeImage = recipes[i]._embedded["wp:featuredmedia"][0].source_url;
-            const recipeImageAltText = recipes[i]._embedded["wp:featuredmedia"][0].alt_text;
-            const recipesTitle = recipes[i].title.rendered;
+        return recipePosts;
 
-            console.log(recipeImageAltText)
 
-            // creating html
-            recipesContainer.innerHTML += `<a href="recipe.html?recipe=${recipes[i].id}"><div class="recipePost">
-                                            <img src="${recipes[i]._embedded["wp:featuredmedia"][0].source_url}" alt="${recipes[i]._embedded["wp:featuredmedia"][0].alt_text}">
-                                            <h3>${recipes[i].title.rendered}</h3>
-                                            </div></a>`;
-      
-        }
 
     } catch (error) { // catching if an error occours
         console.log(error);
@@ -34,4 +33,34 @@ async function getRecipesPosts() {
     }
 }
 
-getRecipesPosts();
+// creating html
+async function getRecipesHTML() {
+
+   recipesContainer.innerHTML = " ";
+
+   var pageNumber = 1;
+
+   function creatHTML(posts) {
+    return  posts.map((post) => {
+        return `<a href="recipe.html?recipe=${post.id}"><div class="recipePost">
+            <img src="${post.imageURL}" alt="${post.imageAltText}" />
+            <h3>${post.title}</h3>
+        </div></a>`
+    })
+    .join("");
+   }
+
+   // first posts
+   const posts = await fetchRecipes(pageNumber, 10);
+   
+   recipesContainer.innerHTML += creatHTML(posts);
+
+    // loading 10 more posts
+    showMore.addEventListener("click", async () => {
+        pageNumber ++;
+        const morePosts = await fetchRecipes(pageNumber, 10);
+        recipesContainer.innerHTML += creatHTML(morePosts);
+    })
+}
+
+getRecipesHTML();
