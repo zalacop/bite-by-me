b7c58a688eb08c2b78b0000ee12993a30f995fae
zalacop <103320914+zalacop@users.noreply.github.com>
2023-05-27 18:18:33 +0200

    called the latest tag for carousel
    made modal responsive on the bigger screens,
    
    fixed some styling for carousel,
    
    split up styles css, first styling went to a new import, to make all the media queries work and not have too long of a file

--- js/imortsJS/url.js
+++ js/imortsJS/url.js
@@ -3,7 +3,7 @@ export const host = "https://www.webupload.no";
 export const wordPress = "/wp-json/wp/v2";
 export const posts = "/posts";
 export const endpoint = "?_embed&";
-export const featured = "?_embed&tags=4";
+export const featured = "?_embed&tags=5";
 
 export const urlPosts = host + wordPress + posts + endpoint; 
 export const urlFeatured = host + wordPress + posts + featured;
